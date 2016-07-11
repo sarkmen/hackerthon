b88f30dae2e352d774da560eafd227cfc7d3ef85
KongHyeontaek <rotanev7@gmail.com>
2016-07-12 00:15:38 +0900

    bootstrap application on template

--- idea/forms.py
+++ idea/forms.py
@@ -1,14 +1,26 @@
 from django import forms
-
+from django.forms.widgets import FileInput
 from .models import Idea, Comment
 
 class IdeaForm(forms.ModelForm):
     class Meta:
         model = Idea
         fields = ['title', 'contents', 'image', ]
+        labels = {
+            'title': '제목',
+            'contents': '내용',
+            'image': '이미지',
+        }
+        widgets = {
+            'title': forms.TextInput(attrs={'placeholder': '제목'}),
+            'contents': forms.Textarea(attrs={'placeholder': '컨텐츠'}),
+        }
 
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['contents', ]
+        widgets = {
+            'contents': forms.Textarea(attrs={'placeholder': '내용 입력', 'rows': 2}),
+        }
