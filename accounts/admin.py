c8790424a31d2f48e473b632b7dd907aa3ee2a9c
beomjun.gim3@gmail.com <beomjun.gim3@gmail.com>
2016-08-10 17:20:04 +0900

    account admin display 수정

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,6 +3,6 @@ from accounts.models import Resume
 # Register your models here.
 
 class ResumeAdmin(admin.ModelAdmin):
-    list_display = ['user', 'name']
+    list_display = ['user', 'name', 'group', 't_size', 'position']
 
 admin.site.register(Resume, ResumeAdmin)
